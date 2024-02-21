b5090c08a9b8dd54ce55c5d37b5843328fb6bf25
Artur Płaczek <arturplaczek@gmail.com>
2024-02-21 20:12:05 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.ugly_english
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:ugly_english/app/app.dart';
+import 'package:ugly_english/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
